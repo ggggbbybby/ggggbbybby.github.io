d7306e2878a0dc419ec094602faa43121fc5cab5
ggggbbybby <rebecca@lendinghome.com>
2017-02-13 22:54:46 -0800

    Revert "Compiled JS Assets"
    This reverts commit 86546c7d3e387372adef43c3e54174653fa07f2e.

--- _assets/javascripts/app.js
+++
@@ -1,7 +0,0 @@
-//= require modernizr-3.3.1.custom.min
-//= require jquery-1.12.0.min
-//= require jquery.dlmenu.min
-//= require jquery.goup.min
-//= require jquery.magnific-popup.min
-//= require scripts
-//= require wow.min
